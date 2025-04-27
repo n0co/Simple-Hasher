b1ae393fe8ba9ffef272614849d22e83636b0fa3
n0co <80924460+n0co@users.noreply.github.com>
2025-04-26 21:07:10 -0400

    Update hash.rb
    added colorize

--- hash.rb
+++ hash.rb
@@ -1,26 +1,27 @@
 require 'digest'
+require 'colorize'
 
 if ARGV[1] == 'sha256'
   digest = Digest::SHA256.hexdigest(ARGV[0])
-  puts 'SHA256 Hashing...'
+  puts 'SHA256 Hashing...'.colorize(:yellow)
   puts
   puts digest
   puts
 elsif ARGV[1] == 'sha512'
   digest = Digest::SHA512.hexdigest(ARGV[0])
-  puts 'SHA512 Hashing...'
+  puts 'SHA512 Hashing...'.colorize(:yellow)
   puts  
   puts digest
   puts
 elsif ARGV[1] == 'md5'
   digest = Digest::MD5.hexdigest(ARGV[0])
-  puts 'MD5 Hashing...'
+  puts 'MD5 Hashing...'.colorize(:yellow)
   puts  
   puts digest
   puts
 else
-  puts 'Script error. Use sha256, sha512, or md5.'
-  puts 'Usage: ruby hash.rb <string> <hash_type>'
+  puts 'Script error. Use sha256, sha512, or md5.'.colorize(:light_red)
+  puts 'Usage: ruby hash.rb <string> <hash_type>'.colorize(:green)
   exit 1
 end
 
